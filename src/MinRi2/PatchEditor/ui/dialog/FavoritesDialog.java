c8ed644e03a5b4f9b5e8a9bba7af060e6944681f
MinRi2 <minri2@qq.com>
2026-04-14 13:08:37 +0800

    💄 重新设计收藏页面

--- src/MinRi2/PatchEditor/ui/dialog/FavoritesDialog.java
+++ src/MinRi2/PatchEditor/ui/dialog/FavoritesDialog.java
@@ -5,21 +5,23 @@ import MinRi2.PatchEditor.ui.*;
 import MinRi2.PatchEditor.NodeFavorites.*;
 import arc.*;
 import arc.graphics.*;
-import arc.math.*;
 import arc.scene.*;
 import arc.scene.ui.*;
 import arc.scene.ui.layout.*;
 import arc.struct.*;
+import arc.struct.ObjectMap.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.gen.*;
+import mindustry.graphics.*;
 import mindustry.ui.*;
 import mindustry.ui.dialogs.*;
 
 public class FavoritesDialog extends BaseDialog{
-    private final Table favoritesTable = new Table();
-
     private String searchText = "";
+    private final ObjectSet<String> shownTypes = new ObjectSet<>();
+
+    private final Table favoritesTable = new Table();
 
     public FavoritesDialog(){
         super("@patch-editor.favorites");
@@ -89,53 +91,94 @@ public class FavoritesDialog extends BaseDialog{
     }
 
     private void rebuildFavoritesTable(){
-        favoritesTable.clearChildren();
-        favoritesTable.defaults().pad(4f);
+        final Table table = favoritesTable;
+        table.clear();
+        table.top().defaults().pad(4f);
 
         Seq<FavoriteField> favorites = NodeFavorites.all().select(favorite ->
         Strings.matches(searchText, favorite.displayName())
         || Strings.matches(searchText, favorite.id));
 
         if(favorites.isEmpty()){
-            favoritesTable.add("@favorites.empty").pad(16f).color(Color.lightGray);
+            table.add("@favorites.empty").pad(16f).color(Color.lightGray);
             return;
         }
 
+        OrderedMap<String, Seq<FavoriteField>> mapped = new OrderedMap<>();
         for(FavoriteField favorite : favorites){
-            favoritesTable.table(Tex.whiteui, row -> {
-                row.table(info -> {
-                    info.left().defaults().left().growX();
-                    info.add(favorite.displayName()).pad(8f).tooltip(favorite.id);
-
-                    String note = FieldNotes.getNote(favorite.id);
-                    if(note != null){
-                        info.row();
-                        info.add(Core.bundle.format("favorites.note.value", note)).padLeft(8f).padRight(8f).padBottom(8f).color(Color.lightGray).wrap().growX();
-                    }
-                }).growX();
-
-                row.table(buttons -> {
-                    buttons.defaults().size(Vars.iconSmall).pad(4f);
-                    buttons.button(Icon.editSmall, Styles.clearNonei, () -> {
-                        EUI.fieldNote.show(favorite.id);
-                    }).tooltip("@patch-editor.note.edit");
-                    buttons.button(Icon.copySmall, Styles.clearNonei, () -> {
-                        Core.app.setClipboardText(favorite.id);
-                        EUI.infoToast("@favorites.copy-id.succeed");
-                    }).tooltip("@favorites.copy-id");
-                    buttons.button(Icon.cancelSmall, Styles.clearNonei, () -> {
-                        NodeFavorites.remove(favorite.id);
-                        rebuildFavoritesTable();
-                    }).tooltip("@favorites.remove");
-                }).pad(4f);
-
-                row.image().width(4f).color(Color.darkGray).growY().right();
-                row.row();
-                Cell<?> horizontalLine = row.image().height(4f).color(Color.darkGray).growX();
-                horizontalLine.colspan(row.getColumns());
-            }).color(EPalettes.gray).growX();
-            favoritesTable.row();
+            mapped.get(favorite.ownerType, Seq::new).add(favorite);
         }
+
+        boolean first = true;
+        int shownCount = 0;
+        shownTypes.clear();
+        for(Entry<String, Seq<FavoriteField>> entry : mapped){
+            String type = entry.key;
+            Seq<FavoriteField> fields = entry.value;
+
+            if(shownCount < 100) shownTypes.add(type);
+
+            table.table(top -> {
+                top.image().color(Pal.darkerGray).width(32f).padRight(8f);
+                top.add(type).labelAlign(Align.left).color(EPalettes.type).minWidth(64f);
+                top.add("(" + fields.size + ")").color(EPalettes.grayFront).padLeft(4f);
+                top.image().color(Pal.darkerGray).padLeft(8f).padRight(16f).growX();
+                top.button(Icon.eyeSmall, Styles.clearTogglei, () -> {
+                    if(!shownTypes.add(type)){
+                        shownTypes.remove(type);
+                    }
+                }).size(Vars.iconMed).checked(shownTypes.contains(type));
+            }).padTop(first ? 0f : 16f).growX();
+
+            table.row();
+
+            table.collapser(cont -> {
+                for(FavoriteField field : fields){
+                    cont.table(t -> setupFavoriteFieldTable(t, field)).padBottom(4f).growX();
+                    cont.row();
+                }
+            }, () -> shownTypes.contains(type)).growX();
+
+            table.row();
+
+            shownCount += fields.size;
+            first = false;
+        }
+    }
+
+    private void setupFavoriteFieldTable(Table table, FavoriteField favorite){
+        table.background(Tex.whiteui).setColor(EPalettes.gray);
+
+        table.table(info -> {
+            info.left().defaults().left().growX();
+            info.add(favorite.displayName()).pad(8f).tooltip(favorite.id);
+
+            String note = FieldNotes.getNote(favorite.id);
+            if(note != null){
+                info.row();
+                info.add(Core.bundle.format("favorites.note.value", note)).padLeft(8f).padRight(8f).padBottom(8f).color(Color.lightGray).wrap().growX();
+            }
+        }).growX();
+
+        table.table(buttons -> {
+            buttons.defaults().size(Vars.iconSmall).pad(4f);
+            buttons.button(Icon.editSmall, Styles.clearNonei, () -> {
+                EUI.fieldNote.show(favorite.id);
+            }).tooltip("@patch-editor.note.edit");
+            buttons.button(Icon.copySmall, Styles.clearNonei, () -> {
+                Core.app.setClipboardText(favorite.id);
+                EUI.infoToast("@favorites.copy-id.succeed");
+            }).tooltip("@favorites.copy-id");
+            buttons.button(Icon.cancelSmall, Styles.clearNonei, () -> {
+                NodeFavorites.remove(favorite.id);
+                rebuildFavoritesTable();
+            }).tooltip("@favorites.remove");
+        }).pad(4f);
+
+        table.image().width(4f).color(Color.darkGray).growY().right();
+        table.row();
+        Cell<?> horizontalLine = table.image().height(4f).color(Color.darkGray).growX();
+        horizontalLine.colspan(table.getColumns());
     }
 
     private void exportFavorites(){
