e37b2c0ec42f2b8ca98154818cd185c284f141f5
MinRi2 <minri2@qq.com>
2026-05-31 10:06:09 +0800

    ✨ 更好的158支持

--- src/dustdustry/patcheditor/EVars.java
+++ src/dustdustry/patcheditor/EVars.java
@@ -1,15 +1,20 @@
 package dustdustry.patcheditor;
 
 import mindustry.*;
-import mindustry.core.*;
 import mindustry.mod.Mods.*;
 
 public class EVars{
     public static LoadedMod thisMod;
-
-    public static boolean hasAmoType = Version.build < 158;
+    public static boolean hasAmoType;
 
     public static void init(){
         thisMod = Vars.mods.getMod(Main.class);
+
+        try{
+            Class.forName("mindustry.type.AmmoType");
+            hasAmoType = true;
+        }catch(Exception e){
+            hasAmoType = false;
+        }
     }
 }
