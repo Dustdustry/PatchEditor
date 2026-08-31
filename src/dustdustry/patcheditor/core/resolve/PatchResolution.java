c15eba1f5b07d105910f7e5a320e48e5691a6921
MinRi2 <minri2@qq.com>
2026-08-31 20:54:48 +0800

    🎨 解析策略调整

--- src/dustdustry/patcheditor/core/resolve/PatchResolution.java
+++ src/dustdustry/patcheditor/core/resolve/PatchResolution.java
@@ -1,4 +1,18 @@
 package dustdustry.patcheditor.core.resolve;
 
+import dustdustry.patcheditor.core.*;
+import mindustry.ctype.*;
+
 public class PatchResolution extends ResolutionStrategy{
+    @Override
+    public void resolveRoot(ObjectNode node){
+        var map = PatchJsonIO.getNameToType();
+        for(ContentType ctype : ContentType.all){
+            if(map.containsValue(ctype, true)){
+                node.addChild(map.findKey(ctype, true), ctype, ContentType.class, ctype.contentClass, null);
+            }
+        }
+
+        node.addChild("name", "<unnamed>").addSign(ModifierSign.MODIFY);
+    }
 }
--- src/dustdustry/patcheditor/core/resolve/ResolutionStrategy.java
+++ src/dustdustry/patcheditor/core/resolve/ResolutionStrategy.java
@@ -43,14 +43,6 @@ public abstract class ResolutionStrategy{
     );
 
     public void resolveRoot(ObjectNode node){
-        var map = PatchJsonIO.getNameToType();
-        for(ContentType ctype : ContentType.all){
-            if(map.containsValue(ctype, true)){
-                node.addChild(map.findKey(ctype, true), ctype, ContentType.class, ctype.contentClass, null);
-            }
-        }
-
-        node.addChild("name", "<unnamed>").addSign(ModifierSign.MODIFY);
     }
 
     public void addSpecialChildren(ObjectNode node, Object object){
